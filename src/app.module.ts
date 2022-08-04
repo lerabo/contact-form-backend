27b8a1d44e0570506fd962f92f2cbc3aff315e46
Lera Bondarchuk <lerabondarchuk38@gmail.com>
2022-08-04 23:47:34 +0300

    Modified app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,12 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      // host: process.env.DB_HOST,
+      // port: Number(process.env.DB_PORT),
+      // username: process.env.DB_USER,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
       ssl: {
         rejectUnauthorized: false,
       },
