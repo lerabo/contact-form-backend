252fa03f8af251f6507d2ce86f69f2a6b5433ee8
lerabo <62139768+lerabo@users.noreply.github.com>
2022-08-05 13:10:35 +0300

    Remove unnecessary variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,6 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
       url: process.env.DATABASE_URL,
-      // host: process.env.DB_HOST,
-      // port: Number(process.env.DB_PORT),
-      // username: process.env.DB_USER,
-      // password: process.env.DB_PASSWORD,
-      // database: process.env.DB_NAME,
       ssl: {
         rejectUnauthorized: false,
       },
